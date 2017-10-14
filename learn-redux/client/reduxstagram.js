56858aa9b30454f3f8c05348a4d3b3d2610a0be5
Leigh Steiner <steinerleigh@gmail.com>
2017-10-14 15:29:02 -0400

    raven sentry implemented

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -16,6 +16,28 @@ import Photogrid from './components/Photogrid';
 import { Router, Route, IndexRoute, browserHistory} from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
+
+import Raven from 'raven-js';
+import { sentry_url, logException } from'./data/config'
+
+Raven.config(sentry_url, {
+	tags: {
+		git_commit: 'asoihaerg21849', 
+		userLevel: "editor"
+	}
+}).install();
+
+logException(new Error('download failed!'), {
+  email: 'wesbos@gmail.com'
+}); //valuable information 
+
+Raven.captureMessage('something bad happened');
+
+Raven.showReportDialog(); //gives user feedback with information 
+
+//console.log(window.doesNotExist.nope); //create error
+
+
 const router = (
  <Provider store={store}>
   <Router history={history}>
